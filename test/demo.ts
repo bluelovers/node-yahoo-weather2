a4df4d9b512f6a6140a8c52a58bd68a30a30ce73
bluelovers <codelovers@users.sourceforge.net>
2017-11-25 06:05:25 +0800

    . (Step: 5)
    Step: 5
    
    [ M] modified - Changes not staged for commit:
    
    	api.ts
    	index.ts
    	package-lock.json
    	package.json
    	test/demo.ts

--- test/demo.ts
+++ test/demo.ts
@@ -2,15 +2,14 @@
  * Created by user on 2017/11/25/025.
  */
 
-import { getWeather, packWeather } from '..';
+import { getWeather, packWeather, geoTimeZoneId, getWeatherPack } from '..';
+import * as geoTimezone from 'geo-timezone';
+import * as util from 'util';
+import * as moment from 'moment';
 
-getWeather('taipei')
-	.then(function (channel)
-	{
-		return packWeather(channel, {
-			lang: 'zh',
-		});
-	})
+getWeatherPack('taipei', {
+	lang: 'zh',
+})
 	.then(function (channel)
 	{
 		console.log(channel);
