5c95d8c6ec74e355ee78542d8b7cfd7a15ac9067
bluelovers <codelovers@users.sourceforge.net>
2017-11-25 01:47:18 +0800

    init (Step: 4)
    Step: 4
    
    [A ] new file:
    
    	.gitignore
    	.npmignore
    	api.ts
    	locales/index.ts
    	locales/zh.ts
    	package-lock.json

---
+++ locales/index.ts
@@ -0,0 +1,62 @@
+/**
+ * Created by user on 2017/11/25/025.
+ */
+
+/**
+ * https://developer.yahoo.com/weather/documentation.html
+ *
+ * @type {{0: string; 1: string; 2: string; 3: string; 4: string; 5: string; 6: string; 7: string; 8: string; 9: string; 10: string; 11: string; 12: string; 13: string; 14: string; 15: string; 16: string; 17: string; 18: string; 19: string; 20: string; 21: string; 22: string; 23: string; 24: string; 25: string; 26: string; 27: string; 28: string; 29: string; 30: string; 31: string; 32: string; 33: string; 34: string; 35: string; 36: string; 37: string; 38: string; 39: string; 40: string; 41: string; 42: string; 43: string; 44: string; 45: string; 46: string; 47: string; 3200: string}}
+ */
+export const code = {
+	0: 'tornado',
+	1: 'tropical storm',
+	2: 'hurricane',
+	3: 'severe thunderstorms',
+	4: 'thunderstorms',
+	5: 'mixed rain and snow',
+	6: 'mixed rain and sleet',
+	7: 'mixed snow and sleet',
+	8: 'freezing drizzle',
+	9: 'drizzle',
+	10: 'freezing rain',
+	11: 'showers',
+	12: 'showers',
+	13: 'snow flurries',
+	14: 'light snow showers',
+	15: 'blowing snow',
+	16: 'snow',
+	17: 'hail',
+	18: 'sleet',
+	19: 'dust',
+	20: 'foggy',
+	21: 'haze',
+	22: 'smoky',
+	23: 'blustery',
+	24: 'windy',
+	25: 'cold',
+	26: 'cloudy',
+	27: 'mostly cloudy (night)',
+	28: 'mostly cloudy (day)',
+	29: 'partly cloudy (night)',
+	30: 'partly cloudy (day)',
+	31: 'clear (night)',
+	32: 'sunny',
+	33: 'fair (night)',
+	34: 'fair (day)',
+	35: 'mixed rain and hail',
+	36: 'hot',
+	37: 'isolated thunderstorms',
+	38: 'scattered thunderstorms',
+	39: 'scattered thunderstorms',
+	40: 'scattered showers',
+	41: 'heavy snow',
+	42: 'scattered snow showers',
+	43: 'heavy snow',
+	44: 'partly cloudy',
+	45: 'thundershowers',
+	46: 'snow showers',
+	47: 'isolated thundershowers',
+	3200: 'not available',
+};
+
+export default code;
