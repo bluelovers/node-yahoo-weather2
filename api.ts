4f8f636d66543582d45ed0e8d84d3dcc73560b48
bluelovers <codelovers@users.sourceforge.net>
2017-11-27 08:00:24 +0800

    2.0.0 (Step: 1)
    Step: 1
    
    [M ] modified:
    
    	api.ts
    	index.ts
    	package.json
    	test/demo.ts

--- api.ts
+++ api.ts
@@ -17,7 +17,15 @@ export interface IYahoo
 		count: number,
 		created: vDate,
 		lang: string,
-		results: { channel: IChannel }
+		results: IYahooResults | null;
+	};
+}
+
+export interface IYahooResults
+{
+	channel?: IChannel;
+	place?: {
+		woeid: vNumber,
 	};
 }
 
@@ -89,6 +97,8 @@ export interface IForecast
 	text: string;
 }
 
+export type vLocation = string | number;
+
 /**
  *
  * @param {string} location
@@ -96,24 +106,134 @@ export interface IForecast
  * @param options
  * @returns {Promise<IChannel>}
  */
-export async function getWeather(location: string, unit: string | object = 'c', options?): Promise<IChannel>
+export async function getWeather(location: any, unit: string | object = 'c', options?): Promise<IChannel>
 {
 	if (typeof unit == 'object')
 	{
 		[unit, options] = [null, unit];
 	}
 
-	let queryUri = 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where u=\'' + (unit || 'c') + '\' AND woeid in (select woeid from geo.places(1) where text="' + location.toString() + '")&format=json';
+	let query_woeid: string;
 
-	let ret = await fetch
-		.single(queryUri, options)
+	if (location === -1 || location === '-1')
+	{
+		location = await getRandWoeidReal();
+
+		//console.log(location);
+	}
+
+	if (typeof location == 'object' && location.lat && location.long)
+	{
+		location = `(${location.lat},${location.long})`;
+	}
+	else if (isWoeidArray(location))
+	{
+		query_woeid = 'woeid in (' + (location as vLocation[]).join(',') + ')';
+	}
+	else if (Array.isArray(location))
+	{
+		query_woeid = 'woeid in (select woeid from geo.places(1) where text in ("' + location.join('","') + '"))';
+	}
+	else if (isWoeid(location))
+	{
+		query_woeid = 'woeid in (' + location.toString() + ')';
+	}
+
+	if (!query_woeid)
+	{
+		query_woeid = 'woeid in (select woeid from geo.places(1) where text="' + location.toString() + '")';
+	}
+
+	//console.log(query_woeid);
+
+	//let queryUri = 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where u=\'' + (unit || 'c') + '\' AND woeid in (select woeid from geo.places(1) where text="' + location.toString() + '")&format=json';
+
+	let queryUri = 'select * from weather.forecast where u=\'' + (unit || 'c') + '\' AND ' + query_woeid;
+
+	let ret = await fetchYQL(queryUri, options)
 		.then(function (res: IYahoo)
 		{
 			return res.query.results === null ? null : res.query.results.channel;
-		}) as IChannel
+		})
+		.then((channel: any) =>
+		{
+			if (channel == null)
+			{
+				return Promise.reject(`query fail, location: ${location}`);
+			}
+
+			return channel;
+		})
 	;
 
 	return ret;
+}
+
+export function getRandInt(max: number, min: number = 0): number
+{
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+export function getRandWoeid(min = 1, max = 5000000): number
+{
+	return getRandInt(max, min);
+}
+
+export function getRandWoeidReal(min = 1, max = 5000000): Promise<vNumber>
+{
+	let id = [
+			getRandWoeid(min, max),
+			getRandWoeid(min, max),
+			getRandWoeid(min, max),
+			getRandWoeid(min, max),
+			getRandWoeid(min, max),
+		].join(',')
+	;
+
+	return fetchYQL(`select woeid from geo.places(1) where woeid in (${id})`)
+		.then(function (res)
+		{
+			let ret;
+			//console.log(id, res.query.results);
+
+			if (res.query.results != null && (ret = isWoeid(res.query.results.place.woeid)))
+			{
+				return ret;
+			}
+
+			return getRandWoeidReal(min, max);
+		})
+		;
+}
+
+export function fetchYQL(query: string, options?): Promise<IYahoo>
+{
+	return fetch.single(`https://query.yahooapis.com/v1/public/yql?q=${query}&format=json`, options);
+}
+
+export function isWoeidArray(arr: vNumber[]): vNumber[]
+{
+	if (Array.isArray(arr) && arr.length)
+	{
+		for (let v of arr)
+		{
+			if (!isWoeid(v))
+			{
+				return null;
+			}
+		}
+
+		return arr;
+	}
+
+	return null;
+}
+
+export function isWoeid(id: vNumber): vNumber
+{
+	return (id && /^[0-9]+$/.test(id.toString())) ? id : null;
 }
 
 export default getWeather;
